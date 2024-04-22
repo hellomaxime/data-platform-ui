59a267b655b3a303aebbf25dcc4a18a2854fa8a4
hellomaxime <86252821+hellomaxime@users.noreply.github.com>
2024-04-22 14:33:32 +0200

    bind apache superset button

--- frontend/src/components/main-content/RouteService.jsx
+++ frontend/src/components/main-content/RouteService.jsx
@@ -5,6 +5,7 @@ export default function RouteService({openService}) {
         <>
             {openService === "jupyterhub" && <iframe className="w-full flex-grow" src="http://dataplatform.jupyterhub.io" title="jupyterhub"/>}
             {openService === "kubernetes" && <iframe className="w-full flex-grow" src="http://127.0.0.1:8001/api/v1/namespaces/kubernetes-dashboard/services/http:kubernetes-dashboard:/proxy/" title="kubernetes"/>}
+            {openService === "superset" && <iframe className="w-full flex-grow" src="http://dataplatform.superset.io" title="superset"/>}
             {openService === "home" && <Home />}
         </>
     )
--- frontend/src/components/sidebar-menu/SidebarMenu.jsx
+++ frontend/src/components/sidebar-menu/SidebarMenu.jsx
@@ -96,7 +96,7 @@ export default function SidebarMenu({openTerminal, setOpenTerminal, setOpenServi
                         </svg>
                         <span className="ml-2 text-sm font-medium">Apache Kafka</span>
                     </a>
-                    <a className="relative flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300" href="#">
+                    <a onClick={() => setOpenService("superset")} className="relative flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300" href="#">
                         <svg className="w-7 h-7 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3v11.25A2.25 2.25 0 0 0 6 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0 1 18 16.5h-2.25m-7.5 0h7.5m-7.5 0-1 3m8.5-3 1 3m0 0 .5 1.5m-.5-1.5h-9.5m0 0-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6" />
                         </svg>
