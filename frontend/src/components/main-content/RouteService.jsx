0872f1bcbf3899874ec5147e491a9c64240751fe
hellomaxime <86252821+hellomaxime@users.noreply.github.com>
2024-05-02 18:36:15 +0200

    add new page for minio

--- frontend/src/components/main-content/RouteService.jsx
+++ frontend/src/components/main-content/RouteService.jsx
@@ -1,5 +1,6 @@
 import Documentation from "../services/Documentation";
 import Home from "../services/Home";
+import Minio from "../services/Minio";
 
 export default function RouteService({openService}) {
     return (
@@ -8,6 +9,7 @@ export default function RouteService({openService}) {
             {openService === "kubernetes" && <iframe className="w-full flex-grow" src="http://127.0.0.1:8001/api/v1/namespaces/kubernetes-dashboard/services/http:kubernetes-dashboard:/proxy/" title="kubernetes"/>}
             {openService === "superset" && <iframe className="w-full flex-grow" src="http://dataplatform.superset.io" title="superset"/>}
             {openService === "grafana" && <iframe className="w-full flex-grow" src="http://dataplatform.grafana.io" title="grafana"/>}
+            {openService === "minio" && <Minio />}
             {openService === "home" && <Home />}
             {openService === "documentation" && <Documentation/>}
         </>
---
+++ frontend/src/components/services/Minio.jsx
@@ -0,0 +1,11 @@
+
+export default function Minio() {
+    return (
+        <>
+            <div className="w-full flex-grow">
+                <h1 className="text-4xl px-5 pb-2">Minio</h1>
+                <a className="border-2 ml-2" href="http://dataplatform.minio.io" target="_blank">Open MinIO in new window</a>
+            </div>
+        </>
+    )
+}
